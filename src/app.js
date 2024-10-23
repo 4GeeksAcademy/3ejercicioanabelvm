6ed0846dcb3ea7ae5281de2edfdc31c28d1f0c2e
Anabel VM <anabelvivasm@gmail.com>
2024-10-23 20:19:12 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let Quien = ['El gato','Mi amigo','Mi pez','Mi hermano', 'Mi canario'];
+let Que = ['se comió','cayó','pegó','rompió', 'manchó'];
+let Cuando = ['antes de trabajar','justo cuando acabé','mientras cenaba','mientras yo me duchaba'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excuseGenerator(quien, que, cuando){
+    Quien = Math.floor(Math.random() * quien.length);
+    Que = Math.floor(Math.random() * que.length);
+    Cuando = Math.floor(Math.random() * cuando.length);
+    return quien[Quien] + " " + que[Que] + " " + "mi tarea" + " " + cuando[Cuando];
+}
+function onLoad(){
+    var excusa = document.getElementById('excuse');
+    excusa.innerHTML = excuseGenerator(Quien, Que, Cuando);
+}
+window.onload = onLoad;
